e0423e602396d02d6515dc6735813f3efb18ef53
johnnyjyh <sunnet1988@msn.com>
2018-03-21 21:59:01 +0800

    2018.3.21 21:58
    Signed-off-by: johnnyjyh <sunnet1988@msn.com>

---
+++ feature/Classes/GamePlayerScene.h
@@ -0,0 +1,67 @@
+#ifndef __GamePlayerScene_H__
+#define __GamePlayerScene_H__
+#include "Common.h"
+#include "LayerMonster.h"
+#include "LayerTower.h"
+#include "LayerBullet.h"
+#include "CellsLayer.h"
+
+class GamePlayerScene :
+			public Layer
+{
+public:
+			GamePlayerScene();
+			~GamePlayerScene();
+
+			static Scene *createScene();
+			static GamePlayerScene *create();		
+			static bool loadAnimate();
+
+			//初始化游戏内容
+			bool init();
+			//创建炮台
+			bool createTower();
+			//创建怪物，并处理动作 
+			bool createMonster();
+			//创建并绑定子弹
+			bool createAndBindBullet();
+			//退出按钮
+
+			bool initBackGround();
+
+
+			
+	
+
+
+
+			void onEnter();  //预用于内存申请 相关域cocos类
+			void onExit();//预用于内存清理 相关域cocos类
+			void update(float dt);
+			
+						//以下四个继承来的虚函数用于来集成控制函数
+			bool onTouchBegan(Touch *touch, Event *unused_event);
+
+			void onTouchMoved(Touch *touch, Event *unused_event);
+
+			void onTouchEnded(Touch *touch, Event *unused_event);
+
+			void onTouchCancelled(Touch *touch, Event *unused_event);
+			
+
+			LayerTower *_layerTower;
+			LayerMonster *_layerMonster;
+			CellsLayer *_layerCells;
+
+			float _scalesizeX;
+			float _scalesizeY;
+		
+
+
+
+
+			static int gamePath[7];
+
+};
+
+#endif
